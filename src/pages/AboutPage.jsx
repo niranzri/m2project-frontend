551adb224193a07c8f1ec5d707c6b6e77f01b384
Noelia <noeliairanzoribera@gmail.com>
2023-12-30 21:43:58 +0100

    changed text about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,9 +9,9 @@ function AboutPage () {
                 <h1> Elevating Your Expectations Since 1992 </h1>
                 <p> Welcome to FlyZest, a beacon of Spanish aviation. 
                 Founded in 1992 by Jesús Cidoncha and Noelia Iranzo, we are a testament to loyalty and commitment. 
-                Our journey over the decades stands as a testament to our dedication to providing exceptional flight services. 
-                At FlyZest, we soar above the rest, connecting people and places with Spanish warmth and reliability. 
-                Your journey matters to us, and we are committed to making it memorable with FlyZest.</p>
+                Over the decades, our journey has demonstrated our dedication to providing exceptional flight services. 
+                At FlyZest, we soar above the rest, connecting people and places with Spanish warmth and reliability. </p>
+                <p> Your journey matters to us, and we are committed to making it memorable with FlyZest.</p>
                 <div className={classes.imagesCtn}>
                     <div className={classes.imageCtn}>
                         <img src={Noe} alt="image of Jesús" />
