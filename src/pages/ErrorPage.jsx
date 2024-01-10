6cbeb1a0d02a9c6757e25d7c31a095fdffe598ef
Noelia <noeliairanzoribera@gmail.com>
2024-01-10 20:03:31 +0100

    added error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,13 @@
 
+import classes from "../styles/error.module.css";
+
 function ErrorPage () {
     return (
-        <div className="mainCtn"> 
+        <div className="mainCtn error"> 
+            <div className={classes.errorMessage}>
+                <p className={classes.number}> 404 </p>
+                <p className={classes.text}> Page not found </p>
+            </div>
         </div>
     )
 }
