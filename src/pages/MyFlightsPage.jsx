681896297c0881758711681f6eb63be884a3d2e9
Noelia <noeliairanzoribera@gmail.com>
2024-01-10 14:06:00 +0100

    fixed star problem in MyFlights Page

--- src/pages/MyFlightsPage.jsx
+++ src/pages/MyFlightsPage.jsx
@@ -4,6 +4,8 @@ import classes from "../styles/myflights.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+
 function MyFlightsPage() {
   const { flights, toggleSave } = useContext(FlightsContext);
   const [pastFlights, setPastFlights] = useState([]);
@@ -33,62 +35,73 @@ function MyFlightsPage() {
   }, [flights]);
 
   return (
-    <div className={classes.myFlightsContainer}>
+    <div className="mainCtn myFlights">
       <h2>My Flights</h2>
       <div className={classes.flightsSection}>
         <div className={classes.pastFlights}>
           <h3>Past Flights</h3>
           {pastFlights.map((flight) => (
             <div key={flight.id} className={classes.flightCard}>
-              <p className="flight-detail">
-                Departure city : {flight.departure_city}
-              </p>
-              <p className="flight-detail">
-                Departure time : {flight.departure_time}h
-              </p>
-              <p className="flight-detail">
-                Departure airport : {flight.departure_airport}
-              </p>
-              <p className="flight-detail">
-                Arrival city : {flight.arrival_city}
-              </p>
-              <p className="flight-detail">
-                Arrival airport : {flight.arrival_airport}
-              </p>
-              <p className="flight-detail">
-                Arrival time : {flight.arrival_time}h
-              </p>
-              <p className="flight-detail">
-                Flight date : {flight.flight_date}
-              </p>
-              <p className="flight-detail">Airline : {flight.airline}</p>
-              <p className="flight-detail">Price : {flight.price}</p>
-
-              <button
-                type="button"
-                className={classes.btnStar}
-                onClick={() => toggleSave(flight.id)}
-              >
-                {flight.isSaved ? (
-                  <FontAwesomeIcon
-                    icon={solidStar}
-                    size="xl"
-                    style={{ color: "#08225a" }}
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    icon={regularStar}
-                    size="xl"
-                    style={{ color: "#08225a" }}
-                  />
-                )}
-              </button>
+              <div className={classes.contentCtn}>
+                <p>
+                  Departure city: {flight.departure_city}
+                </p>
+                <p>
+                  Departure airport: {flight.departure_airport}
+                </p>
+                <p>
+                  Departure time: {flight.departure_time}h
+                </p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>
+                  Arrival city: {flight.arrival_city}
+                </p>
+                <p>
+                  Arrival airport: {flight.arrival_airport}
+                </p>
+                <p>
+                  Arrival time: {flight.arrival_time}h
+                </p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>
+                  Flight date: {flight.flight_date}
+                </p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>Airline: {flight.airline}</p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>Price: {flight.price} €</p>
+              </div>
+              <div className={classes.starCtn}>
+                <button
+                  type="button"
+                  className={classes.iconStar}
+                  onClick={() => toggleSave(flight.id)}
+                >
+                  {flight.isSaved ? (
+                    <FontAwesomeIcon
+                      icon={solidStar}
+                      size="xl"
+                      style={{ color: "#08225a" }}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={regularStar}
+                      size="xl"
+                      style={{ color: "#08225a" }}
+                    />
+                  )}
+                </button>
+              </div>
               <button
                 type="button"
                 className={classes.btnNote}
                 onClick={() => setEditingNoteId(flight.id)}
               >
-                <FontAwesomeIcon icon={faPencilAlt} /> Add Note
+                <FontAwesomeIcon icon={faPen} /> Add Note
               </button>
               {editingNoteId === flight.id && (
                 <>
@@ -110,54 +123,66 @@ function MyFlightsPage() {
           <h3>Upcoming Flights</h3>
           {upcomingFlights.map((flight) => (
             <div key={flight.id} className={classes.flightCard}>
-              <p className="flight-detail">
-                Departure city : {flight.departure_city}
-              </p>
-              <p className="flight-detail">
-                Departure date : {flight.departure_time}h
-              </p>
-              <p className="flight-detail">
-                Departure airport : {flight.departure_airport}
-              </p>
-              <p className="flight-detail">
-                Arrival city : {flight.arrival_city}
-              </p>
-              <p className="flight-detail">
-                Arrival airport : {flight.arrival_airport}
-              </p>
-              <p className="flight-detail">
-                Arrival date : {flight.arrival_time}h
-              </p>
-              <p className="flight-detail">
-                Flight date : {flight.flight_date}
-              </p>
-              <p className="flight-detail">Airline : {flight.airline}</p>
-              <p className="flight-detail">Price : {flight.price}</p>
-              <button
-                type="button"
-                className={classes.btnStar}
-                onClick={() => toggleSave(flight.id)}
-              >
-                {flight.isSaved ? (
-                  <FontAwesomeIcon
-                    icon={solidStar}
-                    size="xl"
-                    style={{ color: "#08225a" }}
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    icon={regularStar}
-                    size="xl"
-                    style={{ color: "#08225a" }}
-                  />
-                )}
-              </button>
+              <div className={classes.contentCtn}>
+                <p>
+                  Departure city: {flight.departure_city}
+                </p>
+                <p>
+                  Departure airport: {flight.departure_airport}
+                </p>
+                <p>
+                  Departure time: {flight.departure_time}h
+                </p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>
+                  Arrival city: {flight.arrival_city}
+                </p>
+                <p>
+                  Arrival airport: {flight.arrival_airport}
+                </p>
+                <p>
+                  Arrival time: {flight.arrival_time}h
+                </p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>
+                  Flight date: {flight.flight_date}
+                </p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>Airline: {flight.airline}</p>
+              </div>
+              <div className={classes.contentCtn}>
+                <p>Price: {flight.price} €</p>
+              </div>
+              <div className={classes.starCtn}>
+                <button
+                  type="button"
+                  className={classes.iconStar}
+                  onClick={() => toggleSave(flight.id)}
+                >
+                  {flight.isSaved ? (
+                    <FontAwesomeIcon
+                      icon={solidStar}
+                      size="xl"
+                      style={{ color: "#08225a" }}
+                    />
+                  ) : (
+                    <FontAwesomeIcon
+                      icon={regularStar}
+                      size="xl"
+                      style={{ color: "#08225a" }}
+                    />
+                  )}
+                </button>
+              </div>
               <button
                 type="button"
                 className={classes.btnNote}
                 onClick={() => setEditingNoteId(flight.id)}
               >
-                <FontAwesomeIcon icon={faPencilAlt} /> Add Note
+                <FontAwesomeIcon icon={faPen} /> Add Note
               </button>
               {editingNoteId === flight.id && (
                 <>
