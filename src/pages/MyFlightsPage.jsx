617bc0cc71c92e5dad6fb845915871ee85286b8c
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2024-01-11 16:08:57 +0100

    stlye changes myflights

--- src/pages/MyFlightsPage.jsx
+++ src/pages/MyFlightsPage.jsx
@@ -79,22 +79,32 @@ function MyFlightsPage() {
       {!editingNoteId && (
         <button
           type="button"
-          className={classes.btnNote}
+          className={classes.btn}
           onClick={() => setEditingNoteId(flight.id)}
         >
-          <FontAwesomeIcon icon={faPen} /> Add/Edit Note
+          <FontAwesomeIcon icon={faPen} /> {notes[flight.id] ? "Edit" : "Add"}{" "}
+          Travel Note
         </button>
       )}
       {editingNoteId === flight.id && (
         <>
           <textarea
             value={notes[flight.id] || ""}
+            className={classes.input}
             onChange={(e) => handleNoteChange(flight.id, e.target.value)}
           />
-          <button onClick={() => saveNote(flight.id)}>
-            <FontAwesomeIcon icon={faCheck} size="sm" /> Save Note
+          <button
+            type="button"
+            className={classes.btn}
+            onClick={() => saveNote(flight.id)}
+          >
+            <FontAwesomeIcon icon={faCheck} size="sm" /> Save
           </button>
-          <button onClick={() => setEditingNoteId(null)}>
+          <button
+            type="button"
+            className={classes.btn}
+            onClick={() => setEditingNoteId(null)}
+          >
             <FontAwesomeIcon icon={faTimes} size="sm" /> Cancel
           </button>
         </>
@@ -103,10 +113,11 @@ function MyFlightsPage() {
         <div>
           <p>Note: {notes[flight.id]}</p>
           <button
-            className={classes.btnDeleteNote}
+            type="button"
+            className={classes.btn}
             onClick={() => removeNote(flight.id)}
           >
-            <FontAwesomeIcon icon={faTrashAlt} /> Delete Note
+            <FontAwesomeIcon icon={faTrashAlt} /> Delete
           </button>
         </div>
       )}
@@ -121,39 +132,70 @@ function MyFlightsPage() {
           <h3>Past Flights</h3>
           {pastFlights.map((flight) => (
             <div key={flight.id} className={classes.flightCard}>
-              <p>Departure city: {flight.departure_city}</p>
-              <p>Departure airport: {flight.departure_airport}</p>
-              <p>Departure time: {flight.departure_time}h</p>
-              <div className={classes.contentCtn}>
-                <p>Arrival city: {flight.arrival_city}</p>
-                <p>Arrival airport: {flight.arrival_airport}</p>
-                <p>Arrival time: {flight.arrival_time}h</p>
-                <p>Flight date: {formatDate(flight.flight_date)}</p>
-                <p>Airline: {flight.airline}</p>
-                <p>Price: {flight.price} €</p>
+              <div className={classes.topCtn}>
+                <div className={classes.flightInfo}>
+                  <p>
+                    <span> Flight date: </span> {formatDate(flight.flight_date)}
+                  </p>
+                  <div className={classes.contentCtn}>
+                    <p>
+                      <span> Departure city: </span> {flight.departure_city}
+                    </p>
+                    <p>
+                      <span> Departure airport: </span>{" "}
+                      {flight.departure_airport}
+                    </p>
+                    <p>
+                      <span> Departure time: </span>
+                      {flight.departure_time}h
+                    </p>
+                  </div>
+                  <div className={classes.contentCtn}>
+                    <p>
+                      <span> Arrival city: </span> {flight.arrival_city}
+                    </p>
+                    <p>
+                      <span> Arrival airport: </span>
+                      {flight.arrival_airport}
+                    </p>
+                    <p>
+                      <span> Arrival time: </span> {flight.arrival_time}h
+                    </p>
+                  </div>
+                  <p>
+                    {" "}
+                    <span>Airline: </span> {flight.airline}
+                  </p>
+                  <p>
+                    <span>Price: </span>
+                    {flight.price} €
+                  </p>
+                </div>
+                <div className={classes.starCtn}>
+                  <button
+                    type="button"
+                    className={classes.iconStar}
+                    onClick={() => toggleSave(flight.id)}
+                  >
+                    {flight.isSaved ? (
+                      <FontAwesomeIcon
+                        icon={solidStar}
+                        size="xl"
+                        style={{ color: "#08225a" }}
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={regularStar}
+                        size="xl"
+                        style={{ color: "#08225a" }}
+                      />
+                    )}
+                  </button>
+                </div>
               </div>
-              <div className={classes.starCtn}>
-                <button
-                  type="button"
-                  className={classes.iconStar}
-                  onClick={() => toggleSave(flight.id)}
-                >
-                  {flight.isSaved ? (
-                    <FontAwesomeIcon
-                      icon={solidStar}
-                      size="xl"
-                      style={{ color: "#08225a" }}
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={regularStar}
-                      size="xl"
-                      style={{ color: "#08225a" }}
-                    />
-                  )}
-                </button>
+              <div className={classes.bottomCtn}>
+                {renderNoteButtons(flight)}
               </div>
-              {renderNoteButtons(flight)}
             </div>
           ))}
         </div>
@@ -161,39 +203,71 @@ function MyFlightsPage() {
           <h3>Upcoming Flights</h3>
           {upcomingFlights.map((flight) => (
             <div key={flight.id} className={classes.flightCard}>
-              <p>Departure city: {flight.departure_city}</p>
-              <p>Departure airport: {flight.departure_airport}</p>
-              <p>Departure time: {flight.departure_time}h</p>
-              <div className={classes.contentCtn}>
-                <p>Arrival city: {flight.arrival_city}</p>
-                <p>Arrival airport: {flight.arrival_airport}</p>
-                <p>Arrival time: {flight.arrival_time}h</p>
-                <p>Flight date: {formatDate(flight.flight_date)}</p>
-                <p>Airline: {flight.airline}</p>
-                <p>Price: {flight.price} €</p>
+              <div className={classes.topCtn}>
+                <div className={classes.flightInfo}>
+                  <p>
+                    <span> Flight date: </span> {formatDate(flight.flight_date)}
+                  </p>
+                  <div className={classes.contentCtn}>
+                    <p>
+                      <span> Departure city: </span> {flight.departure_city}
+                    </p>
+                    <p>
+                      <span> Departure airport: </span>{" "}
+                      {flight.departure_airport}
+                    </p>
+                    <p>
+                      <span> Departure time: </span>
+                      {flight.departure_time}h
+                    </p>
+                  </div>
+                  <div className={classes.contentCtn}>
+                    <p>
+                      <span> Arrival city: </span> {flight.arrival_city}
+                    </p>
+                    <p>
+                      <span> Arrival airport: </span>
+                      {flight.arrival_airport}
+                    </p>
+                    <p>
+                      <span> Arrival time: </span> {flight.arrival_time}h
+                    </p>
+                  </div>
+                  <p>
+                    {" "}
+                    <span>Airline: </span> {flight.airline}
+                  </p>
+                  <p>
+                    <span>Price: </span>
+                    {flight.price} €
+                  </p>
+                </div>
+
+                <div className={classes.starCtn}>
+                  <button
+                    type="button"
+                    className={classes.iconStar}
+                    onClick={() => toggleSave(flight.id)}
+                  >
+                    {flight.isSaved ? (
+                      <FontAwesomeIcon
+                        icon={solidStar}
+                        size="xl"
+                        style={{ color: "#08225a" }}
+                      />
+                    ) : (
+                      <FontAwesomeIcon
+                        icon={regularStar}
+                        size="xl"
+                        style={{ color: "#08225a" }}
+                      />
+                    )}
+                  </button>
+                </div>
               </div>
-              <div className={classes.starCtn}>
-                <button
-                  type="button"
-                  className={classes.iconStar}
-                  onClick={() => toggleSave(flight.id)}
-                >
-                  {flight.isSaved ? (
-                    <FontAwesomeIcon
-                      icon={solidStar}
-                      size="xl"
-                      style={{ color: "#08225a" }}
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={regularStar}
-                      size="xl"
-                      style={{ color: "#08225a" }}
-                    />
-                  )}
-                </button>
+              <div className={classes.bottomCtn}>
+                {renderNoteButtons(flight)}
               </div>
-              {renderNoteButtons(flight)}
             </div>
           ))}
         </div>
